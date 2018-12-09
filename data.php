d3ef8e8f2578a28928823f12d05c940d0de90107
ShushanikStepanyan <ShushanikStepanyan>
2018-12-09 15:08:53 +0400

    CV

---
+++ data.php
@@ -0,0 +1,121 @@
+
+
+<?php
+
+session_start();
+
+if (isset($_POST['submit'])) {
+
+$dir = "images/" ;
+$file = $dir  . basename ($_FILES["img"]["name"]);
+move_uploaded_file($_FILES["img"]["tmp_name"], $file);
+
+
+	if (!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['date']) &&  !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['adress']) || !empty($_POST['gender']) && !empty($_POST['company']) && !empty($_POST['job']) && !empty($_POST['ddate']) && !empty($_POST['faculty']) && !empty($_POST['institution']) && !empty($_POST['edate'])  || !empty($_POST['degree'])) {
+
+
+    $firstname = $_POST['firstname'];
+    $_SESSION['firstname'] = $firstname;
+
+	$lastname = $_POST['lastname'];
+    $_SESSION['lastname'] = $lastname;
+
+    $date = $_POST['date'];
+    $_SESSION['date'] = $date;
+
+    $email = $_POST['email'];
+    $_SESSION['email'] = $email;
+	
+	$phone = $_POST['phone'];
+    $_SESSION['phone'] = $phone;
+	
+	$adress = $_POST['adress'];
+    $_SESSION['adress'] = $adress;
+
+	$gender = $_POST['gender'];
+	$_SESSION['gender'] = $gender;
+	
+
+	$company = $_POST['company'];
+	$_SESSION['company'] = $company;
+
+	$job = $_POST['job'];
+	$_SESSION['job'] = $job;
+
+	$ddate = $_POST['ddate'];
+	$_SESSION['ddate'] = $ddate;
+
+	$institution = $_POST['institution'];
+	$_SESSION['institution'] = $institution;
+
+	$faculty = $_POST['faculty'];
+	$_SESSION['faculty'] = $faculty;
+
+	$edate = $_POST['edate'];
+	$_SESSION['edate'] = $edate;
+
+	$degre = $_POST['degre'];
+	$_SESSION['degre'] = $degre;
+
+	
+} else {
+		echo "<div class='alert alert-danger'> All fields required </div>" ;
+	exit ;
+} 
+ 
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+</head>
+<body style="background-color: #ededed ">
+
+	<div class = "container mx-auto text-center" style="width: 70%; margin-top: 10%; background-color: #fff; padding: 20px; border-radius: 5px">
+
+	<center>  <u> <h1> Curriculum Vitae </h1></u> 
+
+	</center>
+	<hr>
+
+	<img style = "width:100px ; height:100px ; border-radius: 50px; >" src="<?=$file?>" alt = "img">
+
+
+    <p>Your First name:   <strong> <?= $_SESSION['firstname']?> </strong> </p> 
+      <p>Your Last name: <?= $_SESSION['lastname']  ?> </p>
+      <p>Your Birth Day: <?= $_SESSION['date'] ?> </p>   
+      <p>Your Email: <?= $_SESSION['email']  ?> </p> 
+
+    
+
+
+
+
+</div>
+
+
+
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Doc</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</head>
+<body> 
+
+
+<div style="float:left; margin-top: 5%">
+<button class = "btn1" style ="width:150px; margin-left:90px; background-color: green">Basic Information</button ><br>
+<button class = "btn2" style ="width:150px;margin-left:90px;background-color: rgb(158, 186, 141)">Work Experience</button><br>
+<button class = "btn3" style ="width:150px;margin-left:90px;background-color: rgb(158, 186, 141)">Qualifications</button><br>
+<button class = "btn4" style ="width:150px;margin-left:90px;background-color: rgb(158, 186, 141)">Education</button><br>
+<button class = "btn5" style ="width:150px;margin-left:90px;background-color:rgb(158, 186, 141) ">Interests</button><br>
+<button class = "btn6" style ="width:150px;margin-left:90px;background-color: rgb(158, 186, 141)">References</button><br>
+</div>
+<div class = "container mx-auto" style="float:left; width: 50%;border: solid; color: rgb(141, 168, 153); margin-top: 5%; margin-bottom: 5%">
+<b> <i> Նույնականացման տվյալներ <i> <b>
+
+<form action="data.php" method="post" enctype="multipart/form-data">
+	
+<input class = "form-control" type="text" name="firstname" placeholder="First name" style="margin-top:10px "> <br>
+<input class = "form-control" type="text" name="lastname" placeholder="Last name" style="margin-top:10px "> <br>
+Ծննդյան տարեթիվ  <input class = "form-control" type="date" name="birthdate" placeholder="Birth date" style="margin-top:10px "> <br>
+Հասցե <input class = "form-control" type="text" name="adress" placeholder="Adress" style="margin-top:10px"> <br>
+Էլ․հասցե <input class = "form-control" type="email" name="email" placeholder="Email"><br>
+Հեռախոսահամար <input class = "form-control" type="tel" name="phone" placeholder="Phone"><br>
+<label>
+<input type="radio" name="gender" value="male">Տղամարդ</label> 
+<label>
+<input type="radio" name="gender" value="female" style="margin-left: 15px">Կին</label> <br>
+<input type="file" name = "img" id = "img"> <br> <br>
+
+	<b> <i> Աշխատանքային փորձ <i> <b> <br>
+	<input type="text" name="company" placeholder="Company name" style="float: left; margin-top:10px; margin-left:8px">
+	<input  type="text" name="job" placeholder="Job title" style="float: left ; margin-top:10px ; margin-left:7px "> 
+	<input  type="text" name="ddate" placeholder="Start-End" style="float: left ; margin-top:10px ; margin-left:7px "> <br> <br>
+	 <input class = "form-control" type="text" name="OI" placeholder="Other Information" style="margin-top:15px"> <br> 
+
+	 <b> <i> Կրթություն <i> <b>  <br>
+	<input type="text" name="institution" placeholder="Institution name" style="float: left; margin-top:10px; margin-left:8px">
+	<input  type="text" name="faculty" placeholder="Faculty" style="float: left ; margin-top:10px ; margin-left:7px "> 
+	<input  type="text" name="edate" placeholder="Start-End" style="float: left ; margin-top:10px ; margin-left:7px "> <br> <br>
+
+<label>	<input type="radio" name="degree" value="Bachelor" style = "margin-left:10px" > Բակալավրի աստիճան </label>
+
+<label> <input type="radio" name="degree" value="Master" style = "margin-left:10px"> Մագիստրոսի աստիճան </label>
+
+<label> <input type="radio" name="degree" value="Phd" style = "margin-left:10px"> PhD </label> <br> <b>
+	<input type="submit" name = "submit" value="Save" class="btn btn-success" style= "width : 100%"> <br> <br>
+
+</form>
+
+
+</div>
+
+	
+
+</body>
+</html>
